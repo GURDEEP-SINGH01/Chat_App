73e1c940b6492714e57f28c77da93a14823f4bcc
unknown <gurdeepsingh1431999@gmail.com>
2024-12-26 19:28:28 +0530

    get and add Friends

--- Server/src/Controller/userController.js
+++ Server/src/Controller/userController.js
@@ -29,15 +29,43 @@ exports.signIn = async (req, res) => {
 
 exports.getFriends = async (req, res) => {
     try {
-        const { getConnectedFriends } = req.params
-        const friendsList = []
-        const user = await User.findOne({ _id: getConnectedFriends });
-        for (const friend of user.friends) {
-            const userFriend = await User.findOne({ _id: friend });
-            friendsList.push(userFriend);
-        }
+        const { sender } = req.body
+        const user = await User.findOne({ _id: sender });
+        const friendsList = await Promise.all(
+            user.friends.map((friend) => User.findOne({ _id: friend }))
+        );
         res.status(200).json(friendsList);
     } catch (error) {
+        console.log("Err", error)
         res.status(500).json({ message: 'Server error', error: error.message });
     }
 };
+
+exports.addFriends = async (req, res) => {
+    try {
+        const { sender, receiver } = req.body;
+        const senderId = await User.findOne({ _id: sender });
+        const receiverId = await User.findOne({ _id: receiver });
+
+        if (!senderId || !receiverId) {
+            res.status(404).json({ message: "User not Found" });
+        }
+        console.log(senderId);
+        if (!senderId.friends.includes(receiver)) {
+            senderId.friends.push(receiver);
+            await senderId.save();
+            console.log(senderId);
+        }
+
+        if (!receiverId.friends.includes(sender)) {
+            receiverId.friends.push(sender);
+            await receiverId.save();
+            console.log(receiverId);
+        }
+
+        res.json({ message: "Friend added" });
+    } catch (error) {
+        console.log("Err", error)
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+}
--- Server/src/Routes/userRoutes.js
+++ Server/src/Routes/userRoutes.js
@@ -6,6 +6,8 @@ const messageController = require('../Controller/messagesController');
 
 routes.post('/signup', userController.signUp);
 routes.post('/signin', userController.signIn);
+routes.post('/addFriends', userController.addFriends);
+routes.get('/getFriends', userController.getFriends);
 
 routes.post('/sendMessage', messageController.sendMessages);
 routes.get('/getMessage', messageController.getMessages);
