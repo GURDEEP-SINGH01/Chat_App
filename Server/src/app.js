a3d355c012d39bafdb0b890e94598fc5d69d48a5
unknown <gurdeepsingh1431999@gmail.com>
2025-01-02 23:34:10 +0530

    done

--- Server/src/app.js
+++ Server/src/app.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use('/chatapp', routes);
-console.log("MongoDB URI:", process.env.DB_URI);
+
 mongoose.connect(DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
