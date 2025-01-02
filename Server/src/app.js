c0d4c073dd0ef60790ed7f76f039fad2273bcac0
unknown <gurdeepsingh1431999@gmail.com>
2025-01-02 22:44:24 +0530

    change for mongo

--- Server/src/app.js
+++ Server/src/app.js
@@ -17,7 +17,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/chatapp', routes);
 
-mongoose.connect(DB_URI)
+mongoose.connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('MongoDB connection error:', err));
 
